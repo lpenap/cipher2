2b17a5aa105b1f4b725e7af124abbc5cbf8870e8
Luis Augusto Peña Pereira <lpenap@gmail.com>
2020-12-23 13:25:59 -0300

    Add config and db modules

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron')
-const { Ui } = require('./lib/Ui.js');
-const { Controller } = require('./lib/Controller.js');
+const { Ui } = require('./lib/ui.js');
+const { Controller } = require('./lib/controller.js');
 
 const myUi = new Ui();
 const myController = new Controller(myUi);
